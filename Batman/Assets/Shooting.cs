683ea3b7a9c2069192cf44963a919bb2759b24ed
Manieri <xavier.manieri@DETNSW>
2017-05-23 17:25:00 +1000

    first commit

---
+++ Batman/Assets/Shooting.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour {
+
+	public bool isPressed;
+	public GameObject bulletPrefab;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		isPressed = Input.GetKey (KeyCode.Mouse0);
+	}
+	void FixedUpdate() {
+		if (isPressed == true) {
+			Instantiate (bulletPrefab, transform.position, transform.rotation);
+		}
+	}
+
+	private void OnCollisionEnter(Collision collidedObject) {
+		if (collidedObject.gameObject.tag == "win_condition") {
+			Destroy (collidedObject.gameObject);
+		}
+	}
+}
